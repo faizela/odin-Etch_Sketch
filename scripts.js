41cfc1c59ff4e77f9a6fe456f4807201a0e2f10f
feisal ahmed <feisala132@gmail.com>
2022-05-20 21:18:56 +0100

    Created some divs

--- scripts.js
+++ scripts.js
@@ -1,8 +1,15 @@
-const container = document.querySelector('#container')
+const container = document.querySelector('#container');
 
-const squareContainer = document.createElement('div')
-squareContainer.classList.add = 'squareContainer'
+const squareContainer = document.createElement('div');
+squareContainer.classList.add('squareContainer');
+container.appendChild(squareContainer);
+
+for(let i = 1; i <=16; i++){
+    let squarediv = document.createElement('div')
+    squarediv.classList.add('squarediv')
+    squareContainer.appendChild(squarediv)
+}
 
 
-container.append('squareContainer')
+
 
