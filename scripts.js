77aedd46f54e4fd25c3073aebdb06986ff43d851
feisal ahmed <feisala132@gmail.com>
2022-05-20 20:32:03 +0100

    adding index, scripts and styles files

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('#container')
+
+const squareContainer = document.createElement('div')
+squareContainer.classList.add = 'squareContainer'
+
+
+container.append('squareContainer')
+
