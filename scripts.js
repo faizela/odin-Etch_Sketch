5ddf8e8764759daf5b73758abcb2a02502554151
feisal ahmed <feisala132@gmail.com>
2022-06-01 22:01:44 +0100

    modified removeBorder function

--- scripts.js
+++ scripts.js
@@ -1,16 +1,26 @@
+
 const container = document.querySelector('#container');
 const squareContainer = document.querySelector(".squareContainer")
 
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("cell");
+let Clickbtn = document.getElementById("clickbtn")
+//Clickbtn.addEventListener("click", numofsqs )
 
 
-
+ 
 // Creates a default grid sized 16x16
 function defaultGrid() {
     makeRows(16);
     makeColumns(16);
 }
+
+function makeGrid(rownum, colnum){
+    makeRows(rownum)
+    makeColumns(colnum)
+}
+
+
 
 
 function makeRows (rowNum){
@@ -22,7 +32,7 @@ function makeRows (rowNum){
 
 
 function makeColumns(cellNum) {
-    for (col = 0; col < rows.length; col++){
+    for (let col = 0; col < rows.length; col++){
      for (i= 0; i < cellNum; i++){
      let newcell = document.createElement("div");
      rows[i].appendChild(newcell).className = "cell"
@@ -52,11 +62,42 @@ function mouseOut() {
     }
 }
 
+function numofsqs (){
+    removeBorder()
+    let rownumber = Number(window.prompt("How many rows of squares do you want? "))
+    let colnumber = Number(window.prompt("How many columns of squares do you want? "))
+    if (rownumber > 100 || colnumber > 100 ){
+        alert("Hey That's too much :(")
+    }
+    //showBorder()
+    makeGrid(rownumber,colnumber)
+    mouseOn()
+    mouseOut()
+    
+      
+}
+function removeBorder() {       
+    while (squareContainer.firstChild) {
+        squareContainer.removeChild(squareContainer.firstChild)
+      }                                          
+}
+
+function showBorder() {                       
+    squareContainer.append(rows)                               
+}
 
 
 defaultGrid()
+//makeGrid(0,0)
+//removeBorder()
+Clickbtn.addEventListener("click", numofsqs)
 mouseOn()
 mouseOut()
+//removeBorder()
+//makeGrid(20,20)
+
+
+
 
 
 
